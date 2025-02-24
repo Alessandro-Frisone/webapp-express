5617a4f19fb18e1e0f79bfc6eeb8a06d0a3a12a7
Alessandro-Frisone <alefri2004@gmail.com>
2025-02-24 17:49:25 +0100

    feat: add show routes and FIX routers routes show (:id)

--- controller/movieController.js
+++ controller/movieController.js
@@ -1,34 +1,67 @@
-const connection = require("../data/db")
+const connection = require("../data/db");
 
 //-----INDEX-----//
-const index =(req, res) => {
-    const sql = "SELECT * FROM movies";
+const index = (req, res) => {
+  const sql = "SELECT * FROM movies";
 
-    connection.execute(sql, (err, results) => {
+  connection.execute(sql, (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        error: "Query Error",
+        message: `Database query failed: ${sql}`,
+      });
+    }
+
+    res.json(results);
+  });
+};
+
+//-----SHOW-----//
+const show = (req, res) => {
+    const { id } = req.params;
+    const movieSql = `
+      SELECT * 
+      FROM movies
+      WHERE id = ?`;
+  
+    connection.execute(movieSql, [id], (err, results) => {
+      if (err) {
+        return res.status(500).json({
+          error: "Query Error",
+          message: `Database query failed: ${movieSql}`,
+        });
+      }
+  
+      const movie = results[0];
+  
+      if (!movie) {
+        return res.status(404).json({
+          error: "Not found",
+          message: "Movie not found",
+        });
+      }
+  
+      const reviewsSql = `
+        SELECT * 
+        FROM reviews
+        WHERE movie_id = ?`;
+  
+      connection.execute(reviewsSql, [id], (err, results) => {
         if (err) {
           return res.status(500).json({
             error: "Query Error",
-            message: `Database query failed: ${sql}`,
+            message: `Database query failed: ${reviewsSql}`,
           });
         }
-    
-        res.json(results);
+        movie.reviews = results;
+        res.json(movie);
       });
-    };
-
-
-//-----SHOW-----//
-const show =(req, res) => {
-    
-}
-
+    });
+  };
+  
 
 
 //-----DESTROY-----//
-const destroy =(req, res) => {
-    
-}
-
-
+const destroy = (req, res) => {};
 
 module.exports = { index, show, destroy };
